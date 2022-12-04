746be395e52af159370c95565a7e32c20ae7d718
Nepolean23 <nlean1996@gmail.com>
2022-12-04 18:14:55 +0530

    Code For SinglePlayer Of Starting Position

---
+++ SnakeAndLadder/SnakeAndLadder/UC_1-SinglePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class UC_1_SinglePlayer
+    {
+        public static void SinglePlayer()
+        {
+            Random random= new Random();
+            int dice = random.Next(1, 7); //Generates Random Number
+            if(dice==0)
+            {
+                Console.WriteLine("The Player Ready To Play: "+dice);
+
+            }
+            else
+            {
+                Console.WriteLine("Roll Again");
+            }
+        }
+    }
+}
