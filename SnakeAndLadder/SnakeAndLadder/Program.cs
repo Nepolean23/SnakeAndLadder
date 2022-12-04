c3cd07b0e6cd9abeeb2b77ac5834b348517d162f
Nepolean23 <nlean1996@gmail.com>
2022-12-04 20:19:56 +0530

    Code For Player Number Of Dice Rolled

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -5,3 +5,4 @@ UC_2_PlayerRollsTheDie.DiceRoll();
 UC_3_PlayerChecksForOptions.OptionsChecking();
 UC_4_PlayerAtWinPosition.WinPosition();
 UC_5_ExactWinPosition.WiningPosition();
+UC_6_ReportingNumberOfDiceRolled.NumberOfDiceRolled();
